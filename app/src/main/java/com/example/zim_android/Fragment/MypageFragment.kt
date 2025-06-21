8959770893685520c6ea2d813389c80b83b62ec6
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-06-21 20:03:38 +0900

    #2 image와 스타일 반영

--- app/src/main/java/com/example/zim_android/Fragment/MypageFragment.kt
+++ app/src/main/java/com/example/zim_android/Fragment/MypageFragment.kt
@@ -2,11 +2,8 @@ package com.example.zim_android.Fragment
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.zim_android.R
 
 class MypageFragment: Fragment(R.layout.mypage_fragment){
@@ -18,39 +15,20 @@ class MypageFragment: Fragment(R.layout.mypage_fragment){
         val titleTextView = headerLayout.findViewById<TextView>(R.id.tv_title)
         titleTextView.text = "마이페이지"
 
-        // 이미지 컨테이너 불러오기
-        val imageContainer = view.findViewById<LinearLayout>(R.id.flag_image_container)
+        // 이모지 넣을 텍스트뷰 가져오기
+        val imageContainer = view.findViewById<TextView>(R.id.country_flag_text_1)
+        // 백엔드에서 방문한 나라 리스트 받아와 텍스트에 개수에 따라 추가하는 로직 추가해야함.
 
-        // 백엔드에서 받아온 이미지 리스트 (수는 제한 없음)
-        val imageUrlList = listOf(
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,
-            R.drawable.view_gray,// 11번째 → 무시됨
-        )
+        // 백엔드에서 리스트 받아오기
 
-        // 최대 15개까지만 보여줌
-        val limitedList = imageUrlList.take(15)
+        // 리스트의 길이에 따라 조건 나누기
+        // 10개 이하인 경우
+            // flag_image_container1 에 넣기
+            // imageView3 의 길이 조절
 
-        for (resId in imageUrlList.take(15)) {
-            val imageView = ImageView(requireContext()).apply {
-                layoutParams = LinearLayout.LayoutParams(40, 40).apply {
-                    setMargins(7, 0, 7, 0)
-                }
-                scaleType = ImageView.ScaleType.CENTER_CROP
-                setImageResource(resId)
-            }
-            imageContainer.addView(imageView)
-        }
+        // 10개 이상인 경우
+            // flag_image_container2에 넣기
+            // imageView3의 길이 조절
 
     }
 
