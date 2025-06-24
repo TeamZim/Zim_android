cff30f7bc0a25d09f2df3eee29866c0a69bc49a6
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-06-25 04:34:43 +0900

    #14 다이얼로그 2차 구현
    - 드롭 다운 구현
    - 감정색 선택 하드코딩 구현
    - 버튼 삽입
       onlicklistenr의 경우, 색상 선택 다이얼로그 만든 후, 추가할 예정

--- app/src/main/java/com/example/zim_android/fragment/ViewMapFragment.kt
+++ app/src/main/java/com/example/zim_android/fragment/ViewMapFragment.kt
@@ -2,9 +2,12 @@ package com.example.zim_android.fragment
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.zim_android.R
 import com.example.zim_android.databinding.ViewMapDialog1Binding
@@ -49,6 +52,23 @@ class ViewMapFragment : Fragment(R.layout.view_map_fragment) {
         dialogBinding.dialogExitBtn.setOnClickListener {
             dialog.dismiss()
         }
+
+        dialogBinding.selectColorBtn.setOnClickListener {
+            // 색깔 선택 다이얼로그로 이동
+        }
+
+        val popupBg = dialogBinding.countrListAutoComplete.dropDownBackground
+        Log.d("Dropdown", "Background: $popupBg") // null이면 테마 적용 안 된 것
+
+        // 임시 더미 리스트
+        val countryNames = listOf("Korea", "한국", "일본", "미국", "프랑스", "독일", "중국", "영국", "이탈리아", "스페인", "러시아", "브라질", "캐나다", "멕시코", "사우디아라비아", "태국", "인도", "베트남", "싱가포르", "남아프리카공화국", "스웨덴", "호주", "네덜란드", "뉴질랜드", "노르웨이", "핀란드", "스위스", "포르투갈", "폴란드", "덴마크", "아르헨티나", "칠레", "이집트", "터키", "아랍에미리트", "인도네시아")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, countryNames)
+        dialogBinding.countrListAutoComplete.setAdapter(adapter)
+
+        val selectedCountry = dialogBinding.countrListAutoComplete.text.toString() // 사용자가 선택한 국가명 텍스트로 저장
+
+
+
 
         dialog.window?.setLayout(
             ViewGroup.LayoutParams.MATCH_PARENT,
