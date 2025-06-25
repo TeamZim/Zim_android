af0549d3ec70c60f9278a59d9ef3d3666c29a446
Ryujee Kim <130418731+ryujeeKim@users.noreply.github.com>
2025-06-26 01:27:18 +0900

    뒷면 수정

--- app/src/main/java/com/example/zim_android/Adapter/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/zim_android/Adapter/PhotoGridAdapter.kt
@@ -1,38 +1,31 @@
 package com.example.zim_android.Adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.zim_android.R
+import com.example.zim_android.databinding.ItemPhotoGridBinding
 
 class PhotoGridAdapter(private val photos: List<Int>) :
     RecyclerView.Adapter<PhotoGridAdapter.PhotoViewHolder>() {
 
-    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.findViewById(R.id.grid_image)
-    }
+    inner class PhotoViewHolder(val binding: ItemPhotoGridBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_photo_grid, parent, false)
-        return PhotoViewHolder(view)
+        val binding = ItemPhotoGridBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return PhotoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        holder.image.setImageResource(photos[position])
-
-
-        // ✅ 마지막 사진 흐리게
-        if (position == photos.size - 1) {
-            holder.image.alpha = 0.3f
-        } else {
-            holder.image.alpha = 1.0f
-        }
+        val imageView = holder.binding.gridImage
+        imageView.setImageResource(photos[position])
+        imageView.alpha = if (position == photos.size - 1) 0.3f else 1.0f
     }
 
     override fun getItemCount(): Int = photos.size
 }
-
-
