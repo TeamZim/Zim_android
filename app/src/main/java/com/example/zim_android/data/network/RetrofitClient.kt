66fde641998d75cf5ced472329483a3e219dcde1
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-07-10 16:11:39 +0900

    도메인 주소 수정
    공백이 있어 제거함.

--- app/src/main/java/com/example/zim_android/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/zim_android/data/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = " https://me-mory.mooo.com" // 백엔드로부터 받은 URL
+    private const val BASE_URL = "https://me-mory.mooo.com" // 백엔드로부터 받은 URL
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
