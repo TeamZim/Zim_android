e8e86640aa714bd5e5618fbe5e0aa2a0c603db0b
KANG JIHYE <146721079+tessakang@users.noreply.github.com>
2025-07-10 16:45:10 +0900

    Revert "Origin/feat/diary/48 diary page"

--- app/src/main/java/com/example/zim_android/Adapter/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/zim_android/Adapter/PhotoGridAdapter.kt
@@ -1,38 +1,24 @@
 package com.example.zim_android.Adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.zim_android.R
 import com.example.zim_android.data.model.TripImageResponse
-import com.example.zim_android.databinding.ItemCardBinding
 import com.example.zim_android.databinding.ItemPhotoGridBinding
 
-class PhotoGridAdapter(private val photos: List<Int>,
-                       private val onItemClick: (Int) -> Unit // position 또는 id 전달 - api 연결하며 수정
-    ) :
+class PhotoGridAdapter(private val photos: List<TripImageResponse>) :
     RecyclerView.Adapter<PhotoGridAdapter.PhotoViewHolder>() {
 
     private var lastVisibleIndex: Int = -1
-
-    inner class PhotoViewHolder(val binding: ItemPhotoGridBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(position: Int) {
-            binding.gridImage.setImageResource(photos[position])
-
-            binding.gridImage.setOnClickListener {
-                onItemClick(position)
-            }
-            binding.gridImage.alpha = if (position == lastVisibleIndex) 0.3f else 1.0f
-        }
-    }
-
 
     fun setLastVisibleIndex(index: Int) {
         lastVisibleIndex = index
         notifyDataSetChanged()
     }
+
+    inner class PhotoViewHolder(val binding: ItemPhotoGridBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val binding = ItemPhotoGridBinding.inflate(
@@ -44,6 +30,15 @@ class PhotoGridAdapter(private val photos: List<Int>,
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val imageView = holder.binding.gridImage
+
+        // ✅ Glide를 사용해 URL 로드
+        Glide.with(holder.itemView.context)
+            .load(photos[position].imageUrl)
+            .into(imageView)
+
+        // 선택된 인덱스일 경우 알파값 변경
+        imageView.alpha = if (position == lastVisibleIndex) 0.3f else 1.0f
     }
 
     override fun getItemCount(): Int = photos.size
