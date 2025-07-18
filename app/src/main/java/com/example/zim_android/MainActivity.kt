39b5908d8ced770c6f8ac4d9e18da6a1e996c2c3
Ryujee Kim <130418731+ryujeeKim@users.noreply.github.com>
2025-07-19 03:22:12 +0900

    54 일기 연동 완료

--- app/src/main/java/com/example/zim_android/MainActivity.kt
+++ app/src/main/java/com/example/zim_android/MainActivity.kt
@@ -9,18 +9,24 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private var gotoFragmentName: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setupWithNavController(navController)
-        handleIntent(intent, navController)
 
     }
 
 }
+
+
+
